923f9f463cf809d9150a9f21e1de5e0878f30df7
user1 <user1@DESKTOP-0IFG55Q>
2019-10-18 18:28:05 +0900

    10월18일 숙제

---
+++ day02/src/variable/Test11.java
@@ -0,0 +1,21 @@
+package variable;
+
+import java.lang.*;
+
+public class Test11 {
+	public static void main(String [] asrg) {
+//		숙제		
+//		영화관 이용 요금은 다음과 같습니다.
+//		
+//		성인 10000원
+//		청소년 7000원
+//
+//		성인 2명, 청소년 3명이 영화를 보러가서 제휴카드로 25% 할인을 받앗습니다.
+//		결제금액을 구하여 출력하시오.
+		
+		
+		
+		
+		
+	}
+}
