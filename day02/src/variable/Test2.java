adca90e6b825f676086f8f4285630a960088a9e9
user1 <user1@DESKTOP-0IFG55Q>
2019-10-18 11:26:35 +0900

    Test2
    연습용 업로드

---
+++ day02/src/variable/Test2.java
@@ -0,0 +1,25 @@
+package variable;
+
+import java.lang.*;
+
+public class Test2 {
+	public static void main (String [] asrg) {
+		
+		int minute = 135;
+		int b = 60;
+		System.out.println(minute / b); 
+		System.out.println(minute % b);
+		
+		int second = 16500;
+		int min = second / b;
+		
+//		System.out.println(min);
+		
+		System.out.println( min / b );
+		System.out.println(min % b);
+		
+		
+	}
+	
+	
+}
