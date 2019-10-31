730271307d48852e428501d44eb00b3bf13d092a
김용범 <@DESKTOP-SOC5209>
2019-10-31 21:42:50 +0900

    10월31일

--- day11/src/oop/method2/Student.java
+++ day11/src/oop/method2/Student.java
@@ -5,16 +5,17 @@ public class Student {
 	String name;
 
 	int score1, score2, score3;
-	int total = score1+score2+score3;
-	int avg = total/3;
+	
 	
 	void print() {
+		int total = this.score1 + this.score2 + this.score3;
+		double average = total / 3.0;
 		System.out.println(this.name);
 		System.out.println("국어= "+this.score1);
 		System.out.println("영어= "+this.score2);
 		System.out.println("수학= "+this.score3);
-		System.out.println("총점= "+(this.total));
-		System.out.println("평균= "+(this.avg));
+		System.out.println("총점= "+total);
+		System.out.println("평균= "+average);
 	}
 	void set(String name,int score1, int score2, int score3) 
 	{
